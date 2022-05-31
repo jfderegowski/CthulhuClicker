da7b5adf2d889efae47c17332d7a1dfbdcb468bf
Janek <jasiojfd@gmail.com>
2022-05-31 21:26:59 +0200

    scene update

---
+++ Assets/Scripts/Patterns/PatternItemPreview.cs
@@ -0,0 +1,56 @@
+using System;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Patterns
+{
+    public class PatternItemPreview : MonoBehaviour
+    {
+        [SerializeField] private CharacterDisplay _characterDisplay;
+        [SerializeField] private TextMeshProUGUI _itemTitle;
+        [SerializeField] private TextMeshProUGUI _itemDescription;
+        [SerializeField] private Button _buttonEquip;
+        [SerializeField] private Button _buttonBuy;
+        [SerializeField] private Button _ButtonCloseItemCard;
+        
+        private void Awake()
+        {
+            _ButtonCloseItemCard.onClick.AddListener(()=>Destroy(gameObject));
+            gameObject.transform.GetChild(0).transform.DOScale(1, 0.2f);
+            gameObject.transform.GetChild(1).transform.DOScale(1, 0.2f);
+        }
+
+        public void Import(Sprite clothesImage, ItemType itemType, string itemTitle, string itemDescription,
+            UnityAction buttonEquip, UnityAction buttonBuy )
+        {
+            switch (itemType)
+            {
+                case ItemType.Hat:
+                    _characterDisplay.Hat = clothesImage;
+                    break;
+                case ItemType.Shirt:
+                    _characterDisplay.Shirt = clothesImage;
+                    break;
+                case ItemType.Wings:
+                    _characterDisplay.Wings = clothesImage;
+                    break;
+                case ItemType.Body:
+                    _characterDisplay.Body = clothesImage;
+                    break;
+                case ItemType.Eye:
+                    _characterDisplay.Eye = clothesImage;
+                    break;
+                default: break;
+            }
+
+            _itemTitle.text = itemTitle;
+            _itemDescription.text = itemDescription;
+            
+            _buttonBuy.onClick.AddListener(buttonBuy);
+            _buttonEquip.onClick.AddListener(buttonEquip);
+        }
+    }
+}
